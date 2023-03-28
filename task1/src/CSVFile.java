846a7fe5df3a28e966e0a0c9064dc8312e5bb69b
Darya Evdokimova <evddaryaevg@gmail.com>
2023-03-29 02:28:31 +0700

    fully decomposed methods + added OOP

--- task1/src/CSVFile.java
+++ task1/src/CSVFile.java
@@ -13,34 +13,65 @@ public class CSVFile {
     HashMap<String, Integer> storeNamesAndAmount;
     HashMap<String, Integer> sortNamesByFrequency;
 
+    static int globalAmountOfNames = 0;
     CSVFile() {
         try {
             this.input = new FileReader("/home/dasha/IdeaProjects/task1/src/random_names.txt");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        this.output = null;
 
         this.storeNamesAndAmount = new HashMap<>();
         this.sortNamesByFrequency = new HashMap<>();
 
         try {
-            this.output = new FileWriter(new File("/home/dasha/IdeaProjects/task1/src/result.csv"));
+            this.output = new FileWriter("/home/dasha/IdeaProjects/task1/src/result.csv");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
-    static void sortFreq(CSVFile csvFile) {
-        csvFile.sortNamesByFrequency =
-                 csvFile.storeNamesAndAmount
-                .entrySet()
-                .stream()
-                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
-                        LinkedHashMap::new));
+    static void keepData(CSVFile csvFile) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(csvFile.input);
+            String currString;
+            String[] arrayOfAllStrings;
+
+            while ((currString = bufferedReader.readLine()) != null) {
+                arrayOfAllStrings = currString.split(" ");
+                for (String currLine : arrayOfAllStrings) {
+                    int countFreq = csvFile.storeNamesAndAmount.getOrDefault(currLine, 0);
+                    csvFile.storeNamesAndAmount.put(currLine, countFreq + 1);
+                    globalAmountOfNames++;
+                }
+            }
+            bufferedReader.close();
+        }
+        catch(Exception e) {
+            System.err.println("Error " + e.getMessage());
+        }
     }
 
+    static HashMap<String, Integer> sortFreq(CSVFile csvFile) {
+        return
+             csvFile.storeNamesAndAmount
+            .entrySet()
+            .stream()
+            .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+            .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                    LinkedHashMap::new));
+    }
+
+    void printResultToFile(CSVFile csvFile) throws IOException {
+        csvFile.output.write("Word, ");
+        csvFile.output.write("Frequency, ");
+        csvFile.output.write("ProcFrequency\n");
+
+        for (String name: csvFile.sortNamesByFrequency.keySet()) {
+            double proc = (double)csvFile.sortNamesByFrequency.get(name) / globalAmountOfNames;
+
+            String value = csvFile.sortNamesByFrequency.get(name).toString();
+            csvFile.output.write(name + ", " + value + ", " + proc + "\n");
+        }
+    }
 }
--- task1/src/Main.java
+++ task1/src/Main.java
@@ -4,58 +4,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         WordsGenerator.generateTextWithRandomNames();
 
-        int globalAmountOfNames = 0;
-
         CSVFile csvFile = new CSVFile();
-
-        try {
-            BufferedReader bufferedReader = new BufferedReader(csvFile.input);
-            String currString;
-            String[] arrayOfAllStrings;
-
-            while ((currString = bufferedReader.readLine()) != null) {
-                arrayOfAllStrings = currString.split(" ");
-                for (String currLine : arrayOfAllStrings) {
-                    int countFreq = csvFile.storeNamesAndAmount.getOrDefault(currLine, 0);
-                    csvFile.storeNamesAndAmount.put(currLine, countFreq + 1);
-                    globalAmountOfNames++;
-                }
-            }
-            bufferedReader.close();
-        }
-        catch(Exception e) {
-            System.err.println("Error " + e.getMessage());
-        }
-
-        CSVFile.sortFreq(csvFile);
-
-        String pathOfResultFile = "/home/dasha/IdeaProjects/task1/src/result.csv";
-        File resFile = new File(pathOfResultFile);
-
-        try (FileWriter fileWriter = new FileWriter(resFile)) {
-            fileWriter.write("Word, ");
-            fileWriter.write("Frequency, ");
-            fileWriter.write("ProcFrequency\n");
-
-            for (String name: csvFile.sortNamesByFrequency.keySet()) {
-                double proc = (double)csvFile.sortNamesByFrequency.get(name) / globalAmountOfNames;
-
-                String value = csvFile.sortNamesByFrequency.get(name).toString();
-                fileWriter.write(name + ", " + value + ", " + proc + "\n");
-            }
-        }
-
-
-//            csvFile.output.write("Word, ");
-//            csvFile.output.write("Frequency, ");
-//            csvFile.output.write("ProcFrequency\n");
-//
-//            for (String name: csvFile.sortNamesByFrequency.keySet()) {
-//                double proc = (double)csvFile.sortNamesByFrequency.get(name) / globalAmountOfNames;
-//
-//                String value = csvFile.sortNamesByFrequency.get(name).toString();
-//                csvFile.output.write(name + ", " + value + ", " + proc + "\n");
-//            }
-
+        CSVFile.keepData(csvFile);
+        csvFile.sortNamesByFrequency = CSVFile.sortFreq(csvFile);
+        csvFile.printResultToFile(csvFile);
+        csvFile.output.close();
     }
 }
