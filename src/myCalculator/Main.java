aad8298c67da6950284f54411075b10422359ad9
Darya Evdokimova <evddaryaevg@gmail.com>
2023-05-03 19:07:56 +0700

    start adding logging

--- src/myCalculator/Main.java
+++ src/myCalculator/Main.java
@@ -1,16 +1,53 @@
 package myCalculator;
 
+//import java.util.logging.LogManager;
+import myCalculator.commands.exceptions.ExceptionCommands;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.*;
+
 public class Main {
+
+    private final static Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         Calculator calculator;
 
-        if (args.length != 0) {
-            String inputPathExpression = args[0];
-            calculator = new Calculator(inputPathExpression);
-        } else {
-            calculator = new Calculator();
+        FileHandler fh;
+        try {
+            fh = new FileHandler("/home/dasha/IdeaProjects/task2/src/MyLogFile.log");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            logger.info("Logger initialized");
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Exception :: ", e);
+        }
+//        try {
+//            InputStream stream = Main.class.getClassLoader().
+//                    getResourceAsStream("logging.properties");
+//            LogManager.getLogManager().readConfiguration(stream);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        LogManager logManager = LogManager.getLogManager();
+
+        if(args.length > 1) {
+            logger.log(Level.WARNING, "Too many args");
+        }
+        else {
+            if (args.length != 0) {
+                String inputPathExpression = args[0];
+                calculator = new Calculator(inputPathExpression);
+            } else {
+                calculator = new Calculator();
+            }
+
+            calculator.calculate();
         }
 
-        calculator.calculate();
+        logger.log(Level.FINE, "Successful work");
+
     }
 }
