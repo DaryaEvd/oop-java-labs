efb11fafc9a0c321d11066bb53f70ce9aae82389
Darya Evdokimova <evddaryaevg@gmail.com>
2023-05-04 01:06:44 +0700

    added push & print tests

---
+++ src/tests/commands/PrintTest.java
@@ -0,0 +1,32 @@
+import myCalculator.Calculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(null);
+    }
+
+    @Test
+    void checkPrint() {
+        Calculator calculator = new Calculator("/home/dasha/IdeaProjects/task2/src/tests/commands/filesForTests/testPrint.txt");
+        calculator.calculate();
+        assertEquals("Command error: Stack is empty\n" + "99.0\n" + "1.0\n", outContent.toString());
+
+    }
+}
