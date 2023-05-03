13c39bc874b92539ca8d173ea16641ea21a22407
Darya Evdokimova <evddaryaevg@gmail.com>
2023-05-03 17:05:53 +0700

    added exceptoin in definition

--- src/myCalculator/commands/Definition.java
+++ src/myCalculator/commands/Definition.java
@@ -2,6 +2,7 @@ package myCalculator.commands;
 
 import myCalculator.Context;
 import myCalculator.commands.exceptions.BadAmountArgs;
+import myCalculator.commands.exceptions.BadDefinition;
 import myCalculator.commands.exceptions.ExceptionCommands;
 import myCalculator.commands.exceptions.Redefinition;
 
@@ -13,10 +14,14 @@ public class Definition extends AbstractCommand {
     @Override
     public void apply() throws ExceptionCommands {
         if(arguments.length != 3) {
-            throw new BadAmountArgs("DEFINE");
+            throw new BadAmountArgs(arguments[0]);
         }
-
-        Double numberToDefine = Double.parseDouble(arguments[2]);
+        Double numberToDefine = 0.0;
+        try {
+            numberToDefine = Double.parseDouble(arguments[2]);
+        } catch (NumberFormatException e) {
+            throw new BadDefinition();
+        }
 
         if(context.containsVar(arguments[1])) {
           throw new Redefinition(arguments[1]);
@@ -24,4 +29,5 @@ public class Definition extends AbstractCommand {
         context.getVariable().put(arguments[1], numberToDefine);
 
     }
+
 }
